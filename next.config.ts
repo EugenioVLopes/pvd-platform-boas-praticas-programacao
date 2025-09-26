5e0a94872cb1dc5747021b74bcc0b8584f5f6daf
EUGENIO SANTOS <eugenio.lopes.090@ufrn.edu.br>
2025-09-26 17:15:06 -0300

    feat: adiciona shadcn/ui components e outros arquivos de configuração

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  // Configuração para o Docker
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
